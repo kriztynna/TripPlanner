7fd7fd192389dc72e4cfddefcc99833d4e80113b
Cristina <cristina.colon+git@gmail.com>
2015-08-19 12:32:19 -0400

    new error handlers

--- app.js
+++ app.js
@@ -49,11 +49,30 @@ if (app.get('env') === 'development') {
   });
 }
 
+/*
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+*/
+
+// catch 404 (i.e., no route was hit) and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// handle all errors (anything passed into `next()`)
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    console.log({error: err});
+    res.render('error', {
     message: err.message,
     error: {}
   });
