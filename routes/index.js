c596d78d3e8a8d9c0e3574f9b1398a2e6e1b2c51
Cristina <cristina.colon+git@gmail.com>
2015-08-19 14:11:23 -0400

    test that main route lists all businesses in the db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var collections = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var keys = Object.keys(collections);
+	var actions = [];
+	for (var i=0;i<keys.length;i++) {
+		actions.push(collections[keys[i]].find({}));
+	}
+	var output = [];
+	Promise.all(actions).then(function(results){
+		console.log('all the actions were performed');
+		console.log(results.length);
+		//results = results[1];
+		for (var i=0;i<results.length;i++){
+			for (var j=0;j<results[i].length;j++) {
+				var current = results[i][j];
+				console.log(current);
+				output.push({name: current.name});
+			}
+		}
+	});
+	res.render('index', {businesses: output});
 });
 
 module.exports = router;
