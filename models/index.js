a1384e279b5ff297bbef59af986411da3c74397f
Cristina <cristina.colon+git@gmail.com>
2015-08-19 13:38:15 -0400

    db running and seeded

--- models/index.js
+++ models/index.js
@@ -1,12 +1,11 @@
 var mongoose = require('mongoose');
 // Notice the `mongodb` protocol; Mongo is basically a kind of server,
 // which handles database requests and sends responses. It's async!
-mongoose.connect('mongodb://localhost/wikistack');
+mongoose.connect('mongodb://localhost/tripplanner');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongodb connection error:'));
 
-
-var Place = new mongoose.Schema({
+var placeSchema = new mongoose.Schema({
 	address: {type: String}, // street address
 	city: {type: String}, 
 	state: {type: String},
@@ -14,22 +13,22 @@ var Place = new mongoose.Schema({
 	location: {type: Array}, // two numbers, latitude and longitude
 });
 
-var Hotel = new mongoose.Schema({
+var hotelSchema = new mongoose.Schema({
 	name: {type: String},
-	place:   {type: mongoose.Schema.Types.ObjectId, ref: 'Place'},
+	place: {type: Object},
 	num_stars: { type: Number, min: 1, max: 5 }, // 1-5 stars
 	amenities: {type: Array, get: getCDS} //comma delimited string list
 });
 
-var Activity = new mongoose.Schema({
+var activitySchema = new mongoose.Schema({
 	name: {type: String},
-	place: {type: mongoose.Schema.Types.ObjectId, ref: 'Place'},
+	place: {type: Object},
 	age_range: {type: String}
 });
 
-var Restaurant = new mongoose.Schema({
+var restaurantSchema = new mongoose.Schema({
 	name: {type: String},
-	place: {type: mongoose.Schema.Types.ObjectId, ref: 'Place'},
+	place: {type: Object},
 	cuisines: {type: Array, get: getCDS}, //comma delimited string list
 	price: { type: Number, min: 1, max: 5 } // 1-5 dollar signs
 });
@@ -38,3 +37,15 @@ var Restaurant = new mongoose.Schema({
 function getCDS (arr) {
 	return arr.join(", ");
 }
+
+var Place = mongoose.model('Place', placeSchema);
+var Hotel = mongoose.model('Hotel', hotelSchema);
+var Activity = mongoose.model('Activity', activitySchema);
+var Restaurant = mongoose.model('Restaurant', restaurantSchema);
+
+module.exports = {
+  Place: Place,
+  Hotel: Hotel,
+  Activity: Activity,
+  Restaurant: Restaurant
+};
